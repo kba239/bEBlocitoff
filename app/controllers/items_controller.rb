b5cdebb40be84f66a692c5677dc54ef49a2b78fc
kba239 <adams.kate.b@gmail.com>
2017-09-07 13:39:23 -0700

    BE Blocitoff Mark As Complete WIP

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,17 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+
+    if @item.destroy
+      flash[:notice] = "Item was deleted successfully."
+    else
+      flash[:notice] = "There was an error. Your item was not deleted."
+    end
+    redirect_to root_path
+  end
+
   private
 
   def item_params
